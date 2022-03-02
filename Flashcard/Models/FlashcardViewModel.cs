ee44a8e3cd7e7871b86e363196e1bb9bada87f2c
Patryk <gp46518@zut.edu.pl>
2022-03-02 22:59:25 +0100

    Pierwsze demo projektu

---
+++ Flashcard/Models/FlashcardViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Flashcard.Models
+{
+	public class Flashpage
+	{
+		public string Word { get; set; }
+		public string Description { get; set; }
+		public int Evaluation { get; set; }
+	}
+
+	public class Flashcard {
+		public Flashpage Avers { get; set; }
+		public Flashpage Revers { get; set; }
+	}
+
+	public record FlascardViewModel
+	{
+		public Flashcard FCard { get; set; }
+		public List<Flashcard> FCards { get; set; }
+	}
+}
