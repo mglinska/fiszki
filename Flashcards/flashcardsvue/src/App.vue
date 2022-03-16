0c52e0bfe79d0a2cce16c17a18d93f2463d007d5
Fornalczyk <fk46509@zut.edu.pl>
2022-03-16 15:27:33 +0100

    logowanie&rejestracja V1.0

--- Flashcards/flashcardsvue/src/App.vue
+++ Flashcards/flashcardsvue/src/App.vue
@@ -1,23 +1,86 @@
 <template>
+  <v-card>
+    <v-layout>
+      <v-navigation-drawer
+        floating
+        permanent
+      >
+        <v-list
+          density="compact"
+          nav
+        >
+          <v-list-item prepend-icon="mdi-home" href="#/" title="Home" value="home"></v-list-item>
+          <v-list-item prepend-icon="mdi-information-outline" href="#/about" title="About" value="about"></v-list-item>
+          <v-list-item prepend-icon="mdi-login" href="#/login" title="Log in" value="about"></v-list-item>
+          <v-list-item prepend-icon="mdi-account-plus" href="#/registration" title="Sign up" value="about"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-main>
+        <About v-if="this.currentPath.slice(2) === 'about'" />
+        <Login v-else-if="this.currentPath.slice(2) === 'login'" />
+        <Registration v-else-if="this.currentPath.slice(2) === 'registration'" />
+        <Home v-else />
+      </v-main>
+    </v-layout>
+  </v-card>
+  <!--
   <v-app>
     <v-main>
-      <UserList/>
+      <a href="#/">Home</a> |
+      <a href="#/about">About</a> |
+      <a href="#/login">Login</a> |
+      <a href="#/registration">Register</a>
+      <component :is="currentView" />
     </v-main>
   </v-app>
+  -->
 </template>
 
 <script>
-import UserList from './components/UserList.vue'
+import Home from './HomePage.vue'
+import About from './AboutPage.vue'
+import NotFound from './NotFoundPage.vue'
+import Registration from './RegistrationPage.vue'
+import Login from './LoginPage.vue'
+
+const routes = {
+  '/': Home,
+  '/about': About,
+  '/login': Login,
+  '/registration': Registration
+}
 
 export default {
-  name: 'App',
-
-  components: {
-    UserList,
+  data() {
+    return {
+      currentPath: window.location.hash,
+      type: 0,
+      title: 'Fiszki',
+      openDrawer: false
+    }
   },
-
-  data: () => ({
-    //
-  }),
+  components: {
+    Home,
+    About,
+    Login,
+    Registration,
+  },
+  computed: {
+    currentView() {
+      console.log(this.currentPath.slice(1) || '/')
+      return routes[this.currentPath.slice(1) || '/'] || NotFound
+    },
+  },
+  mounted() {
+    window.addEventListener('hashchange', () => { this.currentPath = window.location.hash
+		})
+  }
 }
 </script>
+
+<style>
+body{
+  padding: 0;
+  margin: 0;
+}
+</style>
---
+++ Flashcards/flashcardsvue/src/RegistrationPage.vue
@@ -0,0 +1,108 @@
+<template>
+ <v-form 
+    id=reg_form
+    ref="form"
+    v-model="valid"
+    lazy-validation
+  >
+    <v-text-field
+      v-model="name"
+      :counter="20"
+      :rules="nameRules"
+      label="Name"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      v-model="email"
+      :rules="emailRules"
+      label="E-mail"
+      required
+    ></v-text-field>
+
+     <v-text-field
+      v-model="password"
+      :rules="passwordRules"
+      :type="password"
+      label="Password"
+      required
+    ></v-text-field>
+
+     <v-text-field
+      v-model="repassword"
+      :rules="repasswordRules"
+      :type="password"
+      label="Repeat password"
+      required
+    ></v-text-field>
+
+    <v-checkbox
+      v-model="checkbox"
+      :rules="[v => !!v || 'You must agree to continue!']"
+      label="I have read and accept the regulations"
+      required
+    ></v-checkbox>
+
+    <v-btn
+      color="success"
+      class="mr-4"
+      @click="validate"
+    >
+      Sign up!
+    </v-btn>
+
+  </v-form>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      name: '',
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => (v && v.length >= 2) || 'Name must be more than 1 characters',
+        v => (v && v.length <= 29) || 'Name must be less than 30 characters',
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+      password: '',
+      passwordRules: [
+        v => !!v || 'Password is required',
+        v => (v && v.length >= 8) || 'Password must be more than 8 characters',
+        v => (v && v.length <= 29) || 'Password must be less than 30 characters',
+      ],
+      repassword: '',
+      checkbox: false,
+    }),
+    computed: {
+      repasswordRules() {
+        return [
+        v => !!v || 'Repeat password is required',
+        (v) => (v === this.password) || 'The given passwords do not match',
+        ]
+      },
+    },
+    methods: {
+      submit() {
+        window.location.href = "#/";
+        alert("the form has been sent");
+        document.getElementById('reg_form').submit()
+      },
+      validate() {
+        this.$refs.form.resetValidation();
+        const prom = this.$refs.form.validate();
+        prom.then((a) => {
+          if(a.valid) {
+            this.submit();
+          }
+        });
+      },
+    },
+  }
+</script>
+
+
+
