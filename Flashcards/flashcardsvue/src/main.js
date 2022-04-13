547e434fe2f39d3dacbd1d95900f6a11089d1373
Fornalczyk <fk46509@zut.edu.pl>
2022-04-13 19:42:23 +0200

    fiszki front - v1

--- Flashcards/flashcardsvue/src/main.js
+++ Flashcards/flashcardsvue/src/main.js
@@ -5,7 +5,6 @@ import { loadFonts } from './plugins/webfontloader'
 import router from './router'
 
 loadFonts()
-
 createApp(App)
   .use(vuetify).use(router)
   .mount('#app')
--- Flashcards/flashcardsvue/src/views/ManageCollection.vue
+++ Flashcards/flashcardsvue/src/views/ManageCollection.vue
@@ -1,46 +1,82 @@
 <template>
-    <div id="temp3">
+    <div id="temp">
         <h1> {{ collName }} </h1>
     </div>
-  
-  <v-responsive
-    class="overflow-y-auto temp2"
-    max-height="1000"
-    width="500"
-  >
-    <div v-for="i in 10" :key="i" class="pa-6 text-center temp">
-      fiszka {{ i }} 
+    <v-btn v-if="this.status === 0" @click="this.status = 1" variant="contained-text" color="green">Utwórz fiszkę</v-btn>
+    <FlashcardAdd v-if="this.status === 1" v-model:status="status"/>
+    <div id="flashcard_container">
+      <v-responsive>
+        <v-card v-for="fc in flashcards" :key="fc.id" class="flashcard">
+          <v-card-text>
+            {{ fc.question }}
+          </v-card-text>
+          <v-card-text>
+            {{ fc.answer }}
+          </v-card-text>
+          <v-card-actions>
+            <v-btn icon="mdi-trash-can" variant="outlined" class="action_delete">
+            </v-btn>
+            <v-btn icon="mdi-pencil" variant="outlined" class="action_edit">
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-responsive>
     </div>
-
-    <v-responsive height="200vh" class="text-center pa-2">
-    </v-responsive>
-  </v-responsive>
 </template>
 
 <script>
+import FlashcardAdd from '../components/FlashcardAdd.vue'
 export default {
     props: ['collName'],
+    data() {
+      return {
+        flashcards: [
+          {id: 1, id_coll: 1, question: 'pies', answer: 'dog'},
+          {id: 2, id_coll: 1, question: 'kot', answer: 'cat'},
+          {id: 3, id_coll: 1, question: 'ropucha', answer: 'toad'},
+          {id: 4, id_coll: 1, question: 'ropucha', answer: 'toad'},
+          {id: 5, id_coll: 1, question: 'ropucha', answer: 'toad'},
+          {id: 6, id_coll: 1, question: 'ropucha', answer: 'toad'},
+          {id: 7, id_coll: 1, question: 'ropucha', answer: 'toad'},
+          {id: 8, id_coll: 1, question: 'ropucha', answer: 'toad'},
+          {id: 9, id_coll: 1, question: 'ropucha', answer: 'toad'},
+          {id: 10, id_coll: 1, question: 'ropucha', answer: 'toad'},
+          {id: 11, id_coll: 1, question: 'ropucha', answer: 'toad'},
+          {id: 12, id_coll: 1, question: 'ropucha', answer: 'toad'},
+          {id: 13, id_coll: 1, question: 'ropucha', answer: 'toad'},
+          {id: 14, id_coll: 1, question: 'ropucha', answer: 'toad'}
+        ],
+        status: 0,
+      }
+    },
+    components: {
+      FlashcardAdd,
+    }
 }
+
+
 </script>
 
 <style scoped>
-.temp {
-    background-color: aqua;
-    width: 80%;
-    margin: auto;
-    margin-top: 2px;
-    margin-bottom: 2px;
-    border: solid 2px;
-    border-radius: 25%;
-}
 
-.temp2 {
-    margin: auto;
-}
-
-#temp3 {
+#temp {
     margin: auto;
     text-align: center;
+}
+
+.flashcard {
+  margin: 5px;
+  background: gainsboro;
+  max-height: 200px;
+}
+
+#flashcard_container {
+  position: absolute;
+    left: 50%;
+    top: 50px;
+    transform: translate(-50%, 0);
+  margin: auto;
+  width: 20%;
 }
 
 </style>
