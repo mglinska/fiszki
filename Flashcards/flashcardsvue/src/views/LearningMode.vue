646c8beb246035b24334f3348edac92bf9b8665b
Fornalczyk <fk46509@zut.edu.pl>
2022-05-02 18:23:41 +0200

    karuzela poprawki

--- Flashcards/flashcardsvue/src/views/LearningMode.vue
+++ Flashcards/flashcardsvue/src/views/LearningMode.vue
@@ -9,21 +9,32 @@
           v-model="currentIndex"
         >
           <v-carousel-item
-            v-for="fc in flashcards"
-            :key="fc.Id_flashcard"
+            v-for="(fc, i) in flashcards"
+            :key="i"
           >
             <div class="d-flex fill-height justify-center align-center">
               <div class="text-h2 flashcard">
+                <span class="counter text-h4"> {{i+1}}/{{this.flashcards.length}}</span>
                 <span v-if="fc.Inverted == false"> {{ fc.Question }} </span>
                 <span v-else> {{ fc.Answer }} </span>
                 <br>
                 <v-btn v-if="fc.Inverted == false" @click="check(fc)" class="check" variant="contained-text" color="white">Sprawdź</v-btn>
                 <v-btn v-else @click="check(fc)" class="check" variant="contained-text" color="white">Wróć</v-btn>
-                <v-btn v-if="fc.Inverted == true" @click="console.log('XD')" class="memorized" variant="contained-text" color="white">Pamiętam</v-btn>
+                <v-btn v-if="fc.Inverted == true" @click="remove(i)" class="memorized" variant="contained-text" color="white">Pamiętam</v-btn>
               </div>
             </div>
-
           </v-carousel-item>
+
+          <v-carousel-item v-if="end == true">
+            <div class="d-flex fill-height justify-center align-center">
+              <div class="text-h2 flashcard">
+                <h4>Udało Ci się ukończyć naukę!</h4>
+                <v-btn @click="this.$router.go(-1)" class="end_button" variant="contained-text" color="white">Wróć do kolekcji</v-btn>
+                <v-btn @click="this.refreshData(); this.end = false;" class="end_button" variant="contained-text" color="white">Ucz się od początku</v-btn>
+              </div>
+            </div>
+          </v-carousel-item>
+
         </v-carousel>
       </div>
     </div>
@@ -45,6 +56,7 @@ export default {
         coll_id: this.collId,
         fc_id: null,
         currentIndex: 0,
+        end: false,
       }
     },
     components: {
@@ -52,7 +64,7 @@ export default {
     },
     watch: {
       currentIndex: function() {
-        console.log('xD')
+        setTimeout(this.reset, 150);
       }
     },
     methods: {
@@ -75,10 +87,28 @@ export default {
         flashcard.Inverted = !flashcard.Inverted;
       },
 
+      remove(index) {
+        if(index == this.flashcards.length - 1) {
+          this.flashcards.splice(index, 1);
+          this.currentIndex = 0;
+        }
+        else {
+          this.flashcards.splice(index, 1);
+        }
+
+        this.checkIfEverythingIsMemorized();
+      },
+
+      reset() {
+        this.flashcards.forEach(function (element) {
+            element.Inverted = false;
+        });
+      },
+
       checkIfEverythingIsMemorized()
       {
         if (this.flashcards.length == 0) {
-          console.log('przejebane')
+          this.end = true;
         }
       },
     },
@@ -125,7 +155,21 @@ export default {
   .check {
     background-color: green;
     margin-top: 10px;
+    width: 100px;
   }
-  
+
+  .end_button {
+    background-color: green;
+    margin-top: 10px;
+    margin-left: 5px;
+    margin-right: 5px;
+    width: 220px;
+  }
+
+  .counter {
+    position: absolute;
+        right: 5px;
+        top: 0;
+  }
 
 </style>
