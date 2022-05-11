70ff6f482217cf9156aa2b1903dc24cf0d97464e
Fornalczyk <fk46509@zut.edu.pl>
2022-05-11 23:22:23 +0200

    pobieranie z bazy informacji o ilosci uzytkownikow, kolekcji i fiszek i umieszczanie tych informacji w homepage

--- Flashcards/flashcardsvue/src/views/HomePage.vue
+++ Flashcards/flashcardsvue/src/views/HomePage.vue
@@ -1,6 +1,9 @@
 <template>
   <Navbar />
   <h1 style="margin: 35px;">Hello {{ user.First_name }}</h1>
+  <h3 style="margin: 35px;">Liczba użytkoników: {{ users_sum }}</h3>
+  <h3 style="margin: 35px;">Liczba utworzonych kolekcji przez naszych użytkoników: {{ collections_sum }}</h3>
+  <h3 style="margin: 35px;">Liczba utworzonych fiszek przez naszych użytkoników: {{ flashcards_sum }}</h3>
 </template>
 
 <script>
@@ -10,6 +13,9 @@ export default {
   data() {
     return {
       user: {First_name: ''},
+      users_sum: '',
+      collections_sum: '',
+      flashcards_sum: '',
     }
   },
     methods: {
@@ -22,12 +28,38 @@ export default {
           console.log(error.message)
         })
     },
+    get_summary_data() {
+      axios.get("http://localhost:5085/api/" + "User")
+        .then((response)=>{
+          this.users_sum = response.data.length;
+        })
+        .catch( function() { 
+          this.users_sum = 'BŁĄD'
+        })
+      
+      axios.get("http://localhost:5085/api/" + "Collection")
+        .then((response)=>{
+          this.flashcards_sum = response.data.length;
+        })
+        .catch( function() { 
+          this.flashcards_sum = 'BŁĄD'
+        })
+
+      axios.get("http://localhost:5085/api/" + "Flashcard")
+        .then((response)=>{
+          this.collections_sum = response.data.length;
+        })
+        .catch( function() { 
+          this.collections_sum = 'BŁĄD'
+        })
+    }
   },
   components: {
     Navbar
   },
   mounted: function() {
       this.get_user();
+      this.get_summary_data();
     }
 }
 </script>
