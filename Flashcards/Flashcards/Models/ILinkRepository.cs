50571b545538d43349aa92c0fd699995481023d1
reset2000 <pp.gorka@gmail.com>
2022-05-26 12:43:10 +0200

    Ciąg dalszy obsługi linków.

---
+++ Flashcards/Flashcards/Models/ILinkRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Flashcards.Models {
+    public interface ILinkRepository {
+        Task<List<Link>> GetAllLinks();
+        Task<Link> GetLinkByUrl(string url);
+        Task<Link> CreateLink( Link link );
+        Task<string> DeleteLink(string url);
+        Task<string> RemoveExpiredLink();
+    }
+}
---
+++ Flashcards/Flashcards/Models/LinkRepository.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MySql.Data.MySqlClient;
+
+namespace Flashcards.Models {
+    public class LinkRepository : ILinkRepository {
+
+        private readonly AppDbContext _context;
+
+        public LinkRepository( AppDbContext context ) {
+            _context = context;
+        }
+
+        public async Task<List<Link>> GetAllLinks() {
+            List<Link> links = await _context.Link.ToListAsync();
+
+            return links;
+        }
+
+        public async Task<Link> GetLinkByUrl( string url ) {
+            var result = await _context.Link.FirstOrDefaultAsync(l => l.Url == url);
+
+            return result;
+        }
+
+        public async Task<Link> CreateLink( Link link ) {
+            var result = await _context.Link.AddAsync(link);
+            await _context.SaveChangesAsync();
+
+            return result.Entity;
+        }
+
+        public async Task<string> DeleteLink( string url ) {
+            try {
+                var result = await _context.Link.FirstOrDefaultAsync(l => l.Url == url);
+
+                if (result != null) {
+                    _context.Link.Remove(result);
+                    await _context.SaveChangesAsync();
+
+                    return "Operation successful!";
+                }
+
+                return "Operation failed!";
+            } catch (Exception ex) {
+                return ex.Message;
+            }
+        }
+
+
+        public async Task<string> RemoveExpiredLink() {
+            try {
+                List<Link> result = await _context.Link.Where(l => l.Expiration_time <= DateTime.Now).ToListAsync();
+
+                if (result != null) {
+                    foreach (var record in result) {
+                        _context.Link.Remove(record);
+                        await _context.SaveChangesAsync();
+                    }
+
+                    return "Operation successful!";
+                }
+
+                return "Operation failed!";
+            } catch (Exception ex) {
+                return ex.Message;
+            }
+        }
+    }
+}
